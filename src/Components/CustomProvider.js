dbf5f7293e03c6b05b3a66c3598451bf291bc57a
Fran Gonzalez <fmgarg@gmail.com>
2022-01-27 10:03:51 -0300

    componente cart agrega, suma y elimina items

--- src/Components/CustomProvider.js
+++ src/Components/CustomProvider.js
@@ -9,24 +9,64 @@ const CustomProvider = ({children}) => {
 
     const [carrito, setCarrito] = useState([])
     const [cantidad, setCantidad] = useState(0)
-
+  //  const [InCart, setInCart] = useState(false)
     const isInCart = (id) => {
         const found = carrito.find (item => item.id === id);
-        return found;
-        
+        return found;  
     }
 
-    const agregarProducto = (producto,cantidad) => {
-        if (isInCart(producto.id)){
-            console.log("ya existe")
-            console.log(carrito)
-            setCantidad(cantidad)
-            carrito [producto.cantidad] = setCantidad
+    const agregarProducto = (producto,cantidad) => {        
+        isInCart(producto.id)
+        ?
+        setCarrito(carrito.map((prod) =>{
+            if(prod.id === producto.id){
+                prod.cantidad += cantidad
+            }
+            return prod
+        }))
+        :
+        setCarrito([...carrito,{...producto,cantidad}])        
+        
+        /*const isInCart =  carrito.find (item => item.id === item.id)
+        setInCart(true)
+
+        if (isInCart){
+            console.log("ya existe")            
+            isInCart.cantidad += cantidad
+            setCarrito ([...carrito])
+            
+        }else{
+            
+            setCarrito([...carrito,{...producto,cantidad}])
+            console.log ("agregado")
+            
+        }*/
+
+        /*
+        if((isInCart(producto.id))||(carrito.length !== 0)){
+
+        console.log("el carrito ya tiene productos")
+        //console.log(carrito)
+        const realTimeCart = carrito.map (
+            item =>{
+                const container = {};
+                container.id=item.id;
+                container.title=item.title;
+                container.count=item.count;
+                container.price=item.price;
+                return container
+            }
+        )
+        console.log(realTimeCart)
+        setCarrito([...realTimeCart,{...producto,cantidad}])
+        
         }else{
         setCarrito([...carrito,{...producto,cantidad}])
         setCantidad(cantidad)
         console.log ("agregado")
         }
+       */
+    //console.log(carrito)
     }
 
     const eliminarProducto = (id) => {
